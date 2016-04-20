63e82476319884acd8a68f01d2b1d4431611acd1
Michael Haberler <git@mah.priv.at>
2016-04-20 14:00:55 +0200

    test RCN uboot env

--- include/configs/socfpga_de0_nano_soc.h
+++ include/configs/socfpga_de0_nano_soc.h
@@ -16,6 +16,7 @@
 
 #define CONFIG_CMD_ASKENV
 #define CONFIG_CMD_BOOTZ
+#define CONFIG_SUPPORT_RAW_INITRD
 #define CONFIG_CMD_CACHE
 #define CONFIG_CMD_DFU
 #define CONFIG_CMD_DHCP
@@ -37,7 +38,7 @@
 #define CONFIG_BOOTDELAY	3
 #define CONFIG_BOOTFILE		"fitImage"
 #define CONFIG_BOOTARGS		"console=ttyS0," __stringify(CONFIG_BAUDRATE)
-#define CONFIG_BOOTCOMMAND	"run importuenv; run fpgaconfig; run mmcload; run mmcboot"
+#define CONFIG_BOOTCOMMAND	"run mmcboot"
 #define CONFIG_LOADADDR		0x01000000
 #define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
 
@@ -51,46 +52,95 @@
 
 /* Extra Environment */
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"loadaddr= " __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"ramboot=setenv bootargs " CONFIG_BOOTARGS ";" \
-		"bootm ${loadaddr} - ${fdt_addr}\0" \
-        "mmcloadcmd=ext4load\0" \
+	"console=ttyS0,115200\0" \
+	"loadaddr=0x01000000\0" \
+	"fdt_addr=0x07000000\0" \
+	"fdt_file=soc_system.dtb\0" \
+	"fdtdir=/lib/firmware/socfpga\0" \
+	"rdaddr=0x07080000\0" \
 	"interface=mmc\0" \
-        "bootenv=/boot/uEnv.txt\0" \
-	"bootpart=0:2\0" \
-	"bootimage=/boot/zImage\0" \
-	"fdt_addr=100\0" \
-	"fdtimage=/lib/firmware/socfpga/soc_system.dtb\0" \
-	"fpgaimage=\0" \
-	"fpgadata=0x2000000\0" \
-	"fpgaconfig=load ${interface} ${bootpart} ${fpgadata} ${fpgaimage};" \
-	"fpga load 0 ${fpgadata} ${filesize}\0" \
-	"bootm ${loadaddr} - ${fdt_addr}\0" \
-	"mmcroot=/dev/mmcblk0p2\0" \
-	"mmcboot=setenv bootargs " CONFIG_BOOTARGS \
-		" root=${mmcroot} rw rootwait ${extra_bootargs};" \
-		" echo bootargs=${bootargs}  ${extra_bootargs};" \
-	       "bootz ${loadaddr} - ${fdt_addr}\0" \
-	"loadbootenv=load  ${interface} ${bootpart} ${loadaddr} ${bootenv}\0" \
-	"importbootenv=echo Importing environment from ${bootenv} ...; " \
-		"env import -t -r $loadaddr $filesize\0" \
-        "importuenv=if run loadbootenv; then " \
-		       "echo Loaded environment from ${bootenv};" \
-		       "run importbootenv;" \
+	"optargs=\0" \
+	"cmdline=\0" \
+	"mmcdev=0\0" \
+	"mmcpart=\0" \
+	"mmcroot=/dev/mmcblk0p2 ro\0" \
+	"mmcrootfstype=ext4 rootwait\0" \
+	"mmcargs=setenv bootargs console=${console} " \
+		"${optargs} " \
+		"root=${mmcroot} " \
+		"rootfstype=${mmcrootfstype} " \
+		"${cmdline}\0" \
+	"loadimage=load ${interface} ${bootpart} ${loadaddr} ${bootdir}/${bootfile}\0" \
+	"loadrd=load ${interface} ${bootpart} ${rdaddr} ${bootdir}/${rdfile}; setenv rdsize ${filesize}\0" \
+	"loadfdt=echo loading ${fdtdir}/${fdt_file} ...;  load ${interface} ${bootpart} ${fdt_addr} ${fdtdir}/${fdt_file}\0" \
+	"mmcboot=${interface} dev ${mmcdev}; " \
+		"if ${interface} rescan; then " \
+			"echo Checking for: /boot/uEnv.txt ...;" \
+			"for i in 2 3 4 5 6 7 ; do " \
+				"setenv mmcpart ${i};" \
+				"setenv bootpart ${mmcdev}:${mmcpart};" \
+				"if test -e ${interface} ${bootpart} /boot/uEnv.txt; then " \
+					"load ${interface} ${bootpart} ${loadaddr} /boot/uEnv.txt;" \
+					"env import -t ${loadaddr} ${filesize};" \
+					"echo Loaded environment from /boot/uEnv.txt;" \
+					"if test -n ${dtb}; then " \
+						"setenv fdt_file ${dtb};" \
+						"echo Using: dtb=${fdt_file} ...;" \
+					"fi;" \
+					"echo Checking if uname_r is set in /boot/uEnv.txt...;" \
+					"if test -n ${uname_r}; then " \
+						"echo Running uname_boot ...;" \
+						"setenv mmcroot /dev/mmcblk${mmcdev}p${mmcpart} ro;" \
+						"run uname_boot;" \
+					"fi;" \
+				"fi;" \
+			"done;" \
 		"fi;\0" \
-	"mmcload=mmc rescan;"		    \
-                "if test -n $uenvcmd; then " \
-                        "echo Running uenvcmd: $uenvcmd ...;" \
-                        "run uenvcmd;" \
-                "fi;" \
-	        "if test -n $fpgaimage; then " \
-                        "echo fpgaimage= not defined, skipping FPGA load...;" \
-	        "else " \
-			"echo loading FPGA from ${fpgaimage}:;" \
-			"load ${interface} ${bootpart} ${fpgadata} ${fpgaimage};" \
-                "fi;" \
-		"load ${interface} ${bootpart} ${loadaddr} ${bootimage};" \
-		"load ${interface} ${bootpart} ${fdt_addr} ${fdtimage}\0" \
+	"uname_boot="\
+		"setenv bootdir /boot; " \
+		"setenv bootfile vmlinuz-${uname_r}; " \
+		"if test -e ${interface} ${bootpart} ${bootdir}/${bootfile}; then " \
+			"echo loading ${bootdir}/${bootfile} ...; "\
+			"run loadimage;" \
+			"setenv fdtdir /lib/firmware/socfpga/${uname_r}; " \
+			"if test -e ${interface} ${bootpart} ${fdtdir}/${fdt_file}; then " \
+				"run loadfdt;" \
+			"else " \
+				"setenv fdtdir /lib/firmware/socfpga/; " \
+				"if test -e ${interface} ${bootpart} ${fdtdir}/${fdt_file}; then " \
+					"run loadfdt;" \
+				"else " \
+					"setenv fdtdir /boot/dtbs/${uname_r}; " \
+					"if test -e ${interface} ${bootpart} ${fdtdir}/${fdt_file}; then " \
+						"run loadfdt;" \
+					"else " \
+	                                        "setenv fdtdir /boot/dtbs/; "					\
+ 					        "if test -e ${interface} ${bootpart} ${fdtdir}/${fdt_file}; then " \
+						      "run loadfdt;" \
+					        "else " \
+					              "echo no dtb found in /lib/firmware/socfpga or /boot/dtbs ...; "\
+	                                        "fi;"	 \
+	                                "fi;" \
+				"fi;" \
+			"fi; " \
+			"setenv rdfile initrd.img-${uname_r}; " \
+			"if test -e ${interface} ${bootpart} ${bootdir}/${rdfile}; then " \
+				"echo loading ${bootdir}/${rdfile} ...; "\
+				"run loadrd;" \
+				"if test -n ${uuid}; then " \
+					"setenv mmcroot UUID=${uuid} ro;" \
+				"fi;" \
+				"run mmcargs;" \
+				"echo debug: [${bootargs}] ... ;" \
+				"echo debug: [bootz ${loadaddr} ${rdaddr}:${rdsize} ${fdt_addr}] ... ;" \
+				"bootz ${loadaddr} ${rdaddr}:${rdsize} ${fdt_addr}; " \
+			"else " \
+				"run mmcargs;" \
+				"echo debug: [${bootargs}] ... ;" \
+				"echo debug: [bootz ${loadaddr} - ${fdt_addr}] ... ;" \
+				"bootz ${loadaddr} - ${fdt_addr}; " \
+			"fi;" \
+		"fi;\0"
 
 
 /* The rest of the configuration is shared */
